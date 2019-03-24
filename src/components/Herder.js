0d846c5e4b97ef55f59a7a8423d566cc46eadae6
icezatoo <ice.pp@hotmail.com>
2019-03-24 10:43:58 +0700

    Add icon Cat

--- src/components/Herder.js
+++ src/components/Herder.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TitleHerder = styled.h3`
+  margin-left: 10px;
+  margin-right: 10px;
+`;
 
 const Herder = ({ title }) => (
   <header className="nav">
-    <h3>{title}</h3>
+    <TitleHerder>{title}</TitleHerder>
+    <img
+      width="25"
+      alt="icon-cat"
+      height="25"
+      src="https://img.icons8.com/metro/26/000000/cat-footprint.png"
+    />
   </header>
 );
 
